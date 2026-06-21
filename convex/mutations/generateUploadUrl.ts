bc4eae3b80429fc8552381a7d1de66dc58b16652
Konatham komal eshwara kumar <komaleshwarakumarkonatham@gmail.com>
2026-06-21 13:34:44 +0000

    update 1

--- convex/mutations/generateUploadUrl.ts
+++
@@ -1,6 +0,0 @@
-import { mutationGeneric as mutation } from "convex/server";
-
-export default mutation(async ({ storage }) => {
-  // Return a short-lived upload URL which accepts a POST with the file bytes
-  return await storage.generateUploadUrl();
-});
